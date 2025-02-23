bda91a163c7e01bb68dbb5d6c4b51f1698fd25ad
Nadezhda Oskina <arsentyevanadya@gmail.com>
2025-02-23 20:36:53 +0300

    SQL HW-2 added

--- database/hw_2_oskina_nadezhda/HW_2.sql
+++ database/hw_2_oskina_nadezhda/HW_2.sql
@@ -1,53 +1,53 @@
--- Создание таблицы клиентов.
+-- Создание таблицы клиентов
 create table customer (
-    customer_id int primary key,
-    first_name varchar(50),
-    last_name varchar(50),
-    gender varchar(30),
-    dob varchar(50),
-    job_title varchar(50),
-    job_industry_category varchar(50),
-    wealth_segment varchar(50),
-    deceased_indicator varchar(50),
-    owns_car varchar(30),
-    address varchar(50),
-    postcode varchar(30),
-    state varchar(30),
-    country varchar(30),
-    property_valuation int
+    customer_id INT primary key,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    gender VARCHAR(30),
+    dob VARCHAR(50),
+    job_title VARCHAR(50),
+    job_industry_category VARCHAR(50),
+    wealth_segment VARCHAR(50),
+    deceased_indicator VARCHAR(50),
+    owns_car VARCHAR(30),
+    address VARCHAR(50),
+    postcode VARCHAR(30),
+    state VARCHAR(30),
+    country VARCHAR(30),
+    property_valuation INT
 );
 
--- Создание таблицы транзакций.
+-- Создание таблицы транзакций
 create table transaction (
-    transaction_id int primary key,
-    product_id int,
-    customer_id int null,
+    transaction_id INT primary key,
+    product_id INT,
+    customer_id INT NULL,
     transaction_date VARCHAR(30),
-    online_order varchar(30),
-    order_status varchar(30),
-    brand varchar(30),
-    product_line varchar(30),
-    product_class varchar(30),
-    product_size varchar(30),
-    list_price float,
-    standard_cost float,
+    online_order VARCHAR(30),
+    order_status VARCHAR(30),
+    brand VARCHAR(30),
+    product_line VARCHAR(30),
+    product_class VARCHAR(30),
+    product_size VARCHAR(30),
+    list_price FLOAT,
+    standard_cost FLOAT,
     foreign key (customer_id) references customer (customer_id)
 );
 
--- Вывод всех записей таблицы клиентов.
+-- Вывод всех записей таблицы клиентов
 select * from customer c ;
 
--- Вывод всех записей таблицы транзакций.
-select * from transaction t  ;
+-- Вывод всех записей таблицы транзакций
+select * from transaction t ;
 
 -- Вывести все уникальные бренды, у которых стандартная стоимость выше 1500 долларов.
 select distinct brand from "transaction" t 
-where t.standard_cost > 1500;
+where t.standard_cost > 1500 ;
 
 -- Вывести все подтвержденные транзакции за период '2017-04-01' по '2017-04-09' включительно.
 select * from "transaction" t 
 where t.transaction_date between '2017-04-01' and '2017-04-09'
-and t.order_status = 'Approved';
+and t.order_status = 'Approved' ;
 
 -- Вывести все профессии у клиентов из сферы IT или Financial Services, которые начинаются с фразы 'Senior'.
 select distinct job_title from customer
@@ -57,7 +57,7 @@ where job_industry_category in ('IT', 'Financial Services') and job_title like '
 select distinct brand 
 from "transaction" t 
 left join customer c on c.customer_id = t.customer_id 
-where c.job_industry_category = 'Financial Services'
+where c.job_industry_category = 'Financial Services' ;
 
 -- Вывести 10 клиентов, которые оформили онлайн-заказ продукции из брендов 'Giant Bicycles', 'Norco Bicycles', 'Trek Bicycles'.
 select distinct t.customer_id, c.first_name, c.last_name
@@ -65,13 +65,13 @@ from "transaction" t
 left join customer c on c.customer_id = t.customer_id 
 where t.brand in ('Giant Bicycles', 'Norco Bicycles', 'Trek Bicycles')
 and t.online_order = 'True'
-limit 10;
+limit 10 ;
 
 -- Вывести всех клиентов, у которых нет транзакций.
 select c.customer_id
 from customer c 
 left join "transaction" t on t.customer_id = c.customer_id 
-where t.transaction_id is null;
+where t.transaction_id is null ;
 
 -- Вывести всех клиентов из IT, у которых транзакции с максимальной стандартной стоимостью.
 select c.customer_id, c.first_name, c.last_name 
@@ -81,7 +81,7 @@ where c.job_industry_category = 'IT'
 and t.standard_cost = 
 	(select max(t2.standard_cost) from "transaction" t2
 	left join customer c2 on c2.customer_id = t2.customer_id 
-	where c2.job_industry_category = 'IT');
+	where c2.job_industry_category = 'IT') ;
 
 -- Вывести всех клиентов из сферы IT и Health, у которых есть подтвержденные транзакции за период '2017-07-07' по '2017-07-17'.
 select distinct c.customer_id, c.first_name, c.last_name 
@@ -89,4 +89,4 @@ from "transaction" t
 left join customer c on c.customer_id = t.customer_id
 where t.transaction_date between '2017-07-07' and '2017-07-17'
 and t.order_status = 'Approved'
-and c.job_industry_category in ('IT', 'Health');
+and c.job_industry_category in ('IT', 'Health') ;
